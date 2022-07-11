f6fde237b801e5b23515458682f7fa8aebcc19ba
ElaXan <107149635+ElaXan@users.noreply.github.com>
2022-07-12 05:51:20 +0700

    Update zex.sh

--- zex.sh
+++ zex.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-command cd
-mitmdump -s proxy.py -k --ssl-insecure --set block_global=false
+installmitmproxy() {
+    if [[ -d "AnimeGamePatch" ]]; then
+        bash AnimeGamePatch/install.sh
+    else
+        bash "Error"
+        exit
+    fi
+}
+
+if [ -f "$HOME/.local/bin/mitmdump" ]; then
+    mitmdump -s proxy.py -k --ssl-insecure --set block_global=false
+else
+    echo -e "Please download mitmproxy\n"
+    sleep 2s
+    read -p "Do you want to download it? (y/n)" asktodownload
+    case $asktodownload in
+        "y" ) installmitmproxy;;
+        "n" ) echo "Okay!";;
+        * ) echo "Wrong imput!!";;
+    esac
+fi
