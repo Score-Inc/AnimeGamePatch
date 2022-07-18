2525a80ff1d7c3397043c9508b44cbd28288c09c
ElaXan <107149635+ElaXan@users.noreply.github.com>
2022-07-19 06:29:41 +0700

    Update zex.sh

--- zex.sh
+++ zex.sh
@@ -925,7 +925,7 @@ whoMadeThis() {
     echo -e "========================================\n               ZEX HERE\n----------------------------------------\n${yellowColor}Script was made by @ElashXander (Telegram)${whiteColor}\n----------------------------------------\n$isThisLatestVersion\n========================================"
 }
 
-versionBash1="1.9"
+versionBash1="2.0"
 
 greenColorBack="$(printf '\033[4;42m')"
 redColorBack="$(printf '\033[4;41m')"
