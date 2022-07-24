687e85c93da45a1ff4c0079eca957ad564881a52
ElaXan <107149635+ElaXan@users.noreply.github.com>
2022-07-25 06:48:15 +0700

    Update zex-test.sh

--- testscript/zex-test.sh
+++ testscript/zex-test.sh
@@ -515,7 +515,7 @@ fixVersionScripts() {
 
 
 # PLEASE DON'T EDIT THIS, THIS LOAD SOME CODE FROM SERVER
-source <(curl -s https://raw.githubusercontent.com/ElaXan/AnimeGamePatch/main/someupdate-test)
+source <(curl -s https://raw.githubusercontent.com/ElaXan/AnimeGamePatch/main/testscript/someupdate-test)
 # source $HOME/AnimeGamePatch/someupdate
 if [[ $versionBashIn1 = "" ]]; then
     echo -e "${redColorBold}Can't connect to server!\n\nScript will run without check Update!${whiteColor}"
